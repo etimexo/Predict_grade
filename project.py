021be9439a49c031b540950ae2b91d4285e3e97f
Elijah Obisesan <elijahobisesan01@gmail.com>
2024-05-28 01:05:56 +0100

    Build, load and save model

--- project.py
+++ project.py
@@ -1,28 +1,54 @@
-import pandas as pd
 import numpy as np
-import sklearn
+import pandas as pd
 from sklearn import linear_model
-import sklearn.model_selection
-df = pd.read_csv("student_mat.csv",  sep=";")
-# df = df[["sex", "G1", "G2", "G3", "studytime", "failures", "absences", "internet", "guardian", "freetime", "romantic"]]
-df = df[["G1", "G2", "G3", "studytime", "failures"]]
+import sklearn
+from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pickle
+
+# style.use("ggplot")
+
+df = pd.read_csv("student_mat.csv", sep=";")
 
 predict = "G3"
-X = np.array(df.drop(columns=[predict]))
-y = np.array(df[predict])
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
-linear = linear_model.LinearRegression()
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-predictions = linear.predict(x_test) # Gets a list of all predictions
-print(acc)
 
-for x in range(len(predictions)):
-    # print(predictions[x], x_test[x], y_test[x])
-    pass
+df = df[["G1", "G2", "absences","failures", "studytime","G3"]]
+df = shuffle(df) # Optional
 
-# print(acc)
-# print('Coefficient: \n', linear.coef_) # These are each slope value
-# print('Intercept: \n', linear.intercept_) # This is the intercept
+x = np.array(df.drop(columns=[predict]))
+y =np.array(df[predict])
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-# print(df.head())
+# Train the model 20 times for the best score and pass as the one we're using for prediction, saving as "best"
+best = 0
+for _ in range(20):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
+
+    linear = linear_model.LinearRegression()
+
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print("Accuracy: " + str(acc))
+
+    if acc > best:
+        best = acc
+        print("the prediction we're making use of is " + str(best))
+        with open("studentgrades.pickle", "wb") as f:
+            pickle.dump(linear, f)
+
+# load and save model
+pickle_in = open("studentgrades.pickle", "rb")
+linear = pickle.load(pickle_in)
+predicted= linear.predict(x_test)
+for x in range(len(predicted)):
+    # pass
+    print(predicted[x], x_test[x], y_test[x])
+
+# Drawing and plotting model
+plot = "failures"
+plt.scatter(df[plot], df["G3"])
+plt.legend(loc=4)
+plt.xlabel(plot)
+plt.ylabel("Final Grade")
+plt.show()
